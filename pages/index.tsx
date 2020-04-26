95a4800aad80ca37f909719e8efdc2641ffec5c3
Ryan Hay <ryanhay@au.ibm.com>
2020-04-26 12:53:18 +1000

    Build 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">Next.js App - Build 5</h1>
+        <h1 className="title">App - Build 1</h1>
       </main>
 
       <style jsx>{`
